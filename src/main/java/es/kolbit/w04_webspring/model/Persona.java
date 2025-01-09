4d20d439bc983acdb40daa683e17e6002ff7dbbf
plopezgrieco <plopezgrieco@gmail.com>
2025-01-09 22:51:02 +0100

    inicio

---
+++ src/main/java/es/kolbit/w04_webspring/model/Persona.java
@@ -0,0 +1,92 @@
+package es.kolbit.w04_webspring.model;
+
+import java.util.Objects;
+
+public class Persona {
+
+	private Integer id;
+	private String nombre;
+	private String apellidos;
+	private String telefono;
+	private String domicilio;
+	
+	public Persona(Integer id, String nombre, String apellidos) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+	}
+
+	public Persona(Integer id, String nombre, String apellidos, String telefono, String domicilio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.domicilio = domicilio;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "[" + id + ", " + nombre + ", " + apellidos + ", " + telefono + ", " + domicilio + "]";
+	}
+	
+
+}
